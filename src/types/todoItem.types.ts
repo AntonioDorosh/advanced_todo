2f69830258ea402a262805578e6026282d353e36
AntonioDorosh <W27steelseries123>
2023-08-03 11:22:08 +0300

    check value text

--- src/types/todoItem.types.ts
+++
@@ -1,8 +0,0 @@
-import {TodoTypes} from "./todo.types.ts";
-
-export type TodoItemProps = {
-    todo: TodoTypes[];
-    removeTodo: (id: number) => void;
-    completedTodo: (id: number) => void;
-    editTodo: (id: number, value: string) => void;
-}
