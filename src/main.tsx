f8382b89b86c75df96704ffbf6666a2e3602dcb9
AntonioDorosh <W27steelseries123>
2023-08-21 21:20:22 +0300

    fix some issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import {TodoProvider} from "./context/TodoInputValue.tsx";
 import {MainTodoProvider} from "./context/TodoContext.tsx";
 import {createGlobalStyle} from "styled-components";
